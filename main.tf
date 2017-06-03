fc3a9394d82ab30e08f61820a591de2cb44925a3
Kenneth Powers <ken@kenpowers.net>
2017-06-03 15:21:44 -0400

    Add loadbalancer.

--- main.tf
+++ main.tf
@@ -15,8 +15,13 @@ resource "digitalocean_ssh_key" "default" {
   public_key = "${file(var.public_key_path)}"
 }
 
+resource "digitalocean_tag" "docker_swarm_public" {
+  name = "docker-swarm-public"
+}
+
 resource "digitalocean_droplet" "docker_swarm_manager" {
   name = "docker-swarm-manager"
+  tags = ["${digitalocean_tag.docker_swarm_public.id}"]
   region = "${var.do_region}"
   size = "${var.do_droplet_size}"
   image = "${var.do_image}"
@@ -37,6 +42,7 @@ resource "digitalocean_droplet" "docker_swarm_manager" {
 resource "digitalocean_droplet" "docker_swarm_worker" {
   count = 3
   name = "docker-swarm-worker-${count.index}"
+  tags = ["${digitalocean_tag.docker_swarm_public.id}"]
   region = "${var.do_region}"
   size = "${var.do_droplet_size}"
   image = "${var.do_image}"
@@ -51,5 +57,23 @@ resource "digitalocean_droplet" "docker_swarm_worker" {
     inline = [
       "docker swarm join --token ${data.external.swarm_join_token.result.worker} ${digitalocean_droplet.docker_swarm_manager.ipv4_address_private}:2377"
     ]
+  }
+}
+
+resource "digitalocean_loadbalancer" "public" {
+  name = "docker-swarm-public-loadbalancer"
+  region = "${var.do_region}"
+  droplet_tag = "${digitalocean_tag.docker_swarm_public.name}"
+
+  forwarding_rule {
+    entry_port = 80
+    entry_protocol = "http"
+    target_port = 80
+    target_protocol = "http"
+  }
+
+  healthcheck {
+    port = 22
+    protocol = "tcp"
   }
 }
--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "manager_public_ip" {
 output "manager_private_ip" {
   value = "${digitalocean_droplet.docker_swarm_manager.ipv4_address_private}"
 }
+
+output "loadbalancer_public_ip" {
+  value = "${digitalocean_loadbalancer.public.ip}"
+}
