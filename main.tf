90ea37b91af493204226a4cc45cd8ef69d17caf0
Kenneth Powers <ken@kenpowers.net>
2017-06-02 21:03:01 -0400

    Add swarm worker provisioning.

--- main.tf
+++ main.tf
@@ -2,6 +2,14 @@ provider "digitalocean" {
   token = "${var.do_token}"
 }
 
+data "external" "swarm_worker_token" {
+  program = ["./get-worker-join-token.sh"]
+  query = {
+    private_key_path = "${var.private_key_path}"
+    host = "${digitalocean_droplet.docker_swarm_manager.ipv4_address}"
+  }
+}
+
 resource "digitalocean_ssh_key" "default" {
   name = "${var.key_name}"
   public_key = "${file(var.public_key_path)}"
@@ -22,6 +30,25 @@ resource "digitalocean_droplet" "docker_swarm_manager" {
   provisioner "remote-exec" {
     inline = [
       "docker swarm init --advertise-addr ${digitalocean_droplet.docker_swarm_manager.ipv4_address_private}"
+    ]
+  }
+}
+
+resource "digitalocean_droplet" "docker_swarm_worker" {
+  name = "docker-swarm-worker"
+  region = "${var.do_region}"
+  size = "${var.do_droplet_size}"
+  image = "${var.do_image}"
+  ssh_keys = ["${digitalocean_ssh_key.default.id}"]
+  private_networking = true
+
+  provisioner "remote-exec" {
+    script = "install-docker.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "docker swarm join --token ${data.external.swarm_worker_token.result.token} ${digitalocean_droplet.docker_swarm_manager.ipv4_address_private}:2377"
     ]
   }
 }
--- variables.tf
+++ variables.tf
@@ -6,6 +6,10 @@ variable "public_key_path" {
   description = "Path to the SSH public key to be used for authentication"
 }
 
+variable "private_key_path" {
+  description = "Path to the SSH private key to be used for authentication"
+}
+
 variable "key_name" {
   description = "Name of the key on Digital Ocean"
   default = "terraform"
